2d2d2045176831fc8dd0cfb24b275b9afb87dfcd
PalmShift <rafaelmontillana12@gmail.com>
2024-11-28 20:55:41 +0800

    first commit

---
+++ app/Filament/Widgets/CustomerChart.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Filament\Widgets\ChartWidget;
+
+class CustomerChart extends ChartWidget
+{
+    protected static ?string $heading = 'Reservations';
+    protected static string $color = 'info';
+
+    protected function getData(): array
+    {
+        $data = Trend::model(Customer::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Reservation',
+                    'data' => $data->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        ];
+
+
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
