f0a8e543c23df31c20e29b0c04682e4255278c44
PalmShift <rafaelmontillana12@gmail.com>
2024-11-29 22:58:58 +0800

    test

--- app/Filament/Widgets/CustomerChart.php
+++ app/Filament/Widgets/CustomerChart.php
@@ -33,7 +33,7 @@ class CustomerChart extends ChartWidget
                 'data' => $data->map(fn ($item) => $item->aggregate),
             ],
         ],
-        'labels' => $data->map(fn ($item) => $item->month),
+        'labels' => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     ];
 
 
