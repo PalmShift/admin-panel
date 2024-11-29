2d93e726598a73af3ee2a1dfd329845215ee5f75
PalmShift <rafaelmontillana12@gmail.com>
2024-11-29 21:37:13 +0800

    asdasd

--- app/Filament/Widgets/CustomerChart.php
+++ app/Filament/Widgets/CustomerChart.php
@@ -14,30 +14,32 @@ class CustomerChart extends ChartWidget
     protected static string $color = 'info';
 
 
-    public function getMonthlyReservations()
+    protected function getData(): array
     {
         $data = Trend::model(Customer::class)
-            ->dateColumn('date')
-            ->between(
-                start: now()->startOfYear(),
-                end: now()->endOfYear(),
-            )
-            ->perMonth()
-            ->count();
+        ->dateColumn('date')
+        ->between(
+            start: now()->startOfYear(),
+            end: now()->endOfYear(),
+        )
+        ->perMonth()
+        ->count();
 
-        $labels = collect(range(1, 12))->map(fn($month) => now()->startOfYear()->addMonths($month - 1)->format('M'));
+    $labels = collect(range(1, 12))->map(fn($month) => now()->startOfYear()->addMonths($month - 1)->format('M'));
 
-        \Log::info('Trend Data:', $data->toArray());
+    \Log::info('Trend Data:', $data->toArray());
 
-        return [
-            'datasets' => [
-                [
-                    'label' => 'Reservations',
-                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
-                ],
+    return [
+        'datasets' => [
+            [
+                'label' => 'Reservations',
+                'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
             ],
-            'labels' => $labels,
-        ];
+        ],
+        'labels' => $labels,
+    ];
+
+
     }
 
     protected function getType(): string
