9bf206b2681b3d9f1449b098c402e6b1070ac653
PalmShift <rafaelmontillana12@gmail.com>
2024-12-11 18:49:22 +0800

    a

---
+++ app/Filament/Widgets/CustomerWeek.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+namespace App\Filament\Widgets;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Filament\Widgets\ChartWidget;
+
+class CustomerWeek extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        $data = DB::table('customers')
+        ->select(
+            DB::raw('DAYNAME(date) as day'),
+            DB::raw('COUNT(*) as count')
+        )
+        ->groupBy('day')
+        ->orderByRaw('FIELD(DAYNAME(date), "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")')
+        ->get();
+
+    return [
+        'datasets' => [
+            [
+                'label' => 'Reservations by Day of the Week',
+                'data' => $data->map(fn($item) => $item->count),
+            ],
+        ],
+        'labels' => $data->map(fn($item) => $item->day),
+    ];
+    }
+
+    protected function getType(): string
+    {
+        return 'polarArea';
+    }
+}
