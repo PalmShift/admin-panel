5f7ef43f039a632ffde0042dd61987a39de91895
PalmShift <rafaelmontillana12@gmail.com>
2024-12-11 18:32:58 +0800

    a

--- app/Filament/Widgets/CustomerTime.php
+++ app/Filament/Widgets/CustomerTime.php
@@ -2,7 +2,15 @@
 
 namespace App\Filament\Widgets;
 
+namespace App\Filament\Widgets;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use App\Filament\Resources\CustomerResource;
+use App\Models\Customer;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
 use Filament\Widgets\ChartWidget;
+
 
 class CustomerTime extends ChartWidget
 {
@@ -10,8 +18,26 @@ class CustomerTime extends ChartWidget
 
     protected function getData(): array
     {
+        $data = DB::table('customers')
+        ->select(
+            DB::raw('CASE
+                WHEN time BETWEEN "10:00:00" AND "12:00:00" THEN "Morning"
+                WHEN time BETWEEN "12:00:01" AND "17:00:00" THEN "Afternoon"
+                WHEN time BETWEEN "17:00:01" AND "21:00:00" THEN "Evening"
+                ELSE "Night"
+            END as time_of_day'),
+            DB::raw('COUNT(*) as count')
+        )
+        ->groupBy('time_of_day')
+        ->get();
         return [
-            //
+            'datasets' => [
+                [
+                    'label' => 'Reservations by Time of Day',
+                    'data' => $data->map(fn($item) => $item->count),
+                ],
+            ],
+            'labels' => $data->map(fn($item) => $item->time_of_day),
         ];
     }
 
